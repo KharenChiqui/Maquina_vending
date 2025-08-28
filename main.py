453fba26d9d7201b48ff8e0e19e35191d3d8ced7
Miguel Jose Lugo Martinez <67250914+lugozil@users.noreply.github.com>
2025-08-28 01:28:22 -0400

    Pantalla-inicio
    Pantalla inicial modificada, fondo, iconos y logo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from kivy.utils import get_color_from_hex
 
 Window.size = (1024, 600)
 
-LabelBase.register(name='LatoLight', fn_regular='fonts/Lato-Light.ttf')
+LabelBase.register(name='Intro', fn_regular='fonts/Intro.otf')
 
 PRECIO_LITRO = 1000
 SUPERUSER_PASSWORD = "aguaSegura24"
